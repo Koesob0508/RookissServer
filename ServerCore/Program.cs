8daac90616f8d9a1f8e9e5c56f8e4348b2219700
Koesob <epi3706@khu.ac.kr>
2023-02-20 00:20:13 +0900

    SpinLock까지

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,44 +1,64 @@
 ﻿namespace ServerCore
 {
+    class SpinLock
+    {
+        volatile int locked = 0;
+
+        public void Aquire()
+        {
+            while (true)
+            {
+                int expected = 0;
+                int desired = 1;
+                if (Interlocked.CompareExchange(ref locked, desired, expected) == expected)
+                {
+                    break;
+                }
+            }
+
+        }
+
+        public void Release()
+        {
+            locked = 0;
+        }
+    }
+
     internal class Program
     {
-        static int number = 0;
-        static object obj = new object();
+        static int _num = 0;
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
         {
-            for(int i = 0; i < 10000; i++)
+            for (int i = 0; i < 1000000; i++)
             {
-                lock(obj)
-                {
-                    number++;
-                }
-                
-                
+                _lock.Aquire();
+                _num++;
+                _lock.Release();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 1000000; i++)
             {
-                lock(obj)
-                {
-                    number--;
-                }
+                _lock.Aquire();
+                _num--;
+                _lock.Release();
             }
         }
-
         static void Main(string[] args)
         {
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
+
             t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(number);
+            Console.WriteLine(_num);
         }
     }
 }
