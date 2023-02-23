b024cdd7c73ac8d6f80a5acc6ad09498e261617b
Koesob <epi3706@khu.ac.kr>
2023-02-23 15:13:50 +0900

    강의 목차별 관리 시작

---
+++ 05. Listener/ServerCore/Listener.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    internal class Listener
+    {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler = onAcceptHandler;
+
+            // 문지기 교육
+            _listenSocket.Bind(endPoint);
+
+            // 영업 시작
+            _listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+
+        private void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+
+            // false면 바로 처리 완료됐단 소리
+            // true면 보류 처리 됐단 소리
+            // pending의 의미가 보류
+            bool pending = _listenSocket.AcceptAsync(args);
+
+            if (pending == false)
+            {
+                OnAcceptCompleted(null, args);
+                // I/O 작업이 동기적으로 완료된 경우 false입니다.
+                // 이 경우에는 e 매개 변수에서 Completed 이벤트가 발생하지 않으며,
+                // 메서드 호출이 반환된 직후 매개 변수로 전달된 e 개체를 검사하여
+                // 작업 결과를 검색할 수 있습니다.
+            }
+        }
+
+        private void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                // TODO
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+            {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+
+            RegisterAccept(args);
+        }
+    }
+}
---
+++ 05. Listener/ServerCore/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    internal class Program
+    {
+        static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello MMORPG Server!");
+                clientSocket.Send(sendBuff);
+
+                // 쫓아낸다
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            
+            // DNS Domain Name System
+
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddress = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, 7777);
+
+            _listener.Init(endPoint, OnAcceptHandler);
+            Console.WriteLine("영업 중...");
+
+            while (true)
+            {
+                
+            }
+
+        }
+    }
+}
